6bb3df339303f08bc62819a36464e55d214f1948
the-sociophobic <torikowashi@gmail.com>
2023-11-14 04:34:41 +0300

    add skillbox ad remover

--- extension/content.js
+++ extension/content.js
@@ -2,14 +2,14 @@
 // It is responsible for running the 'insertButtonIntoElements' function.
 
 function insertButtonIntoElements() {
-  if (!window.location.href.includes('kinopoisk'))
-    return
-
   const findByClassNames = (classNames) => {
     if (!Array.isArray(classNames))
       return []
     
-    var all_divs = document.getElementsByTagName("div")
+    var all_divs = [
+      ...document.getElementsByTagName('div'),
+      ...document.getElementsByTagName('a'),
+    ]
     var divs = []
   
     for (var i = 0; i < all_divs.length; i++) {
@@ -29,24 +29,26 @@ function insertButtonIntoElements() {
     const divs = findByClassNames(classNames)
 
     if (divs?.[0])
-      divs[0].style.display = "none"
+      divs[0].style.display = 'none'
   }
   const removeAllFoundByClassNames = (classNames) => {
     const divs = findByClassNames(classNames)
 
     divs.forEach(div => {
-      div.style.display = "none"
+      // div.setAttribute('style', 'display: none !important')
+      // div.style.display = 'none'
+      div.remove()
     })
   }
 
-  const removeAds = () => {
-    const promo0 = document.getElementById("page1")
+  const removeKinopoiskAds = () => {
+    const promo0 = document.getElementById('page1')
     if (promo0)
-      promo0.style.display = "none"
+      promo0.style.display = 'none'
   
-    const promo1 = document.getElementById("trailer-promo-block")
+    const promo1 = document.getElementById('trailer-promo-block')
     if (promo1)
-      promo1.parentElement.children[1].style.display = "none"
+      promo1.parentElement.children[1].style.display = 'none'
   
     // REMOVE HOMEPAGE UPPER BANNER
     removeFirstFoundByClassNames(['media-post-embed'])
@@ -59,8 +61,8 @@ function insertButtonIntoElements() {
 
     // REMOVE ADBLOCK WARNING
     removeAllFoundByClassNames([
-      "styles_rootRendered__",
-      "styles_adBlockWarningRoot__",
+      'styles_rootRendered__',
+      'styles_adBlockWarningRoot__',
     ])
 
     const footer = document.getElementById('partial_component__header-topline')
@@ -69,11 +71,11 @@ function insertButtonIntoElements() {
       footer.style.display = 'none'
 
     const divs2 = findByClassNames([
-      "styles_sidebar__",
+      'styles_sidebar__',
     ])
     divs2.forEach(div => [...div.children]
       .filter(child => child.className.includes('styles_sticky__'))
-      .forEach(child => child.style.display = "none")
+      .forEach(child => child.style.display = 'none')
     )
 
     // REVOVE SIDEBAR ON SEARCH PAGE
@@ -89,17 +91,51 @@ function insertButtonIntoElements() {
 
     // REMOVE MOVIE PAGE UPPER BANNER
     removeAllFoundByClassNames(['styles_abbreviated__'])
+
+    // REMOVE HEADER ADDS
+    removeAllFoundByClassNames([
+      'styles_featureMenuContainer__',
+      'styles_plusBadgeCounter__'
+    ])
+
+    // REMOVE FOOTER ADDS
+    if (window.location.href.includes('/film/')) {
+      const footerTag = document.getElementsByTagName('footer')
+      const footerFirstChild = footerTag[0]?.children?.[0]
+
+      if (footerFirstChild?.children[0].classList.contains?.('image'))
+        footerFirstChild.remove()
+    }
+  }
+
+  const removeSkillboxAds = () => {
+    removeAllFoundByClassNames([
+      'universal-notice',
+      'subscribe-popup',
+      'subscribe-success-popup',
+      'cookies',
+      'bg-modal-overlay',
+      'article-banner',
+    ])
+
+    const widgets = [...document.getElementsByTagName('fstrk-widget')]
+
+    widgets.forEach(widget => widget.remove())
   }
 
   const element = findByClassNames(['styles_buttonsContainer__'])?.[0]
   
   if (element) {
-    const button = document.createElement("button")
-    button.textContent = "rutracker.org"
+    const button = document.createElement('button')
+    button.textContent = 'rutracker.org'
     element.appendChild(button)
   }
 
-  setInterval(removeAds, 500)
+  if (window.location.href.includes('kinopoisk'))
+    setInterval(removeKinopoiskAds, 500)
+
+  if (window.location.href.includes('skillbox'))
+    setInterval(removeSkillboxAds, 500)
 }
 
 // Run the function when the content script is injected into a page.
